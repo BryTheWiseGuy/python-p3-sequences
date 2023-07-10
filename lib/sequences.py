26e6fe1f4beec9aa23d6d734e6082d16167504f2
Brian Ashman <brian.ashman@comcast.net>
2023-07-10 09:20:26 -0700

    Complete Sequence Lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = []
+    if length >= 1:
+        fibonacci_list.append(0)
+    if length >= 2:
+        fibonacci_list.append(1)
+    
+    for i in range(2, length):
+        next_fibonacci = fibonacci_list[i - 1] + fibonacci_list[i - 2]
+        fibonacci_list.append(next_fibonacci)
+    
+    print(fibonacci_list)
